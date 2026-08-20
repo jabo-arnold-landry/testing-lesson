213714c712612f99316cb0000916b81314f368af
jabo <arnoldjabo@gmail.com>
2026-08-20 11:37:30 +0200

    fix: remove conflicts in add contact controller

--- src/controllers/add-contacts.controller.ts
+++ src/controllers/add-contacts.controller.ts
@@ -3,21 +3,13 @@ import { Request, Response } from "express";
 import validateEmail from "../../utils/email-validation";
 
 async function addContacts(req: Request, res: Response) {
-  try {
-    const { contactName, phoneNumber, email } = req.body;
-    const isEmailValid = validateEmail(email);
-    if (!isEmailValid) {
-      res.status(400).json({ message: "The email is invalid" });
-      return;
-    }
-    const contact = await Contacts.create({ contactName, phoneNumber, email });
-    return res
-      .status(201)
-      .json({ message: `successfully created ${contact.contactName}` });
-  } catch (err) {
-    console.log(err);
-    return;
-  }
+  const { contactName, phoneNumber, email } = req.body;
+  validateEmail(email);
+
+  const contact = await Contacts.create({ contactName, phoneNumber, email });
+  return res
+    .status(201)
+    .json({ message: `successfully created ${contact.contactName}` });
 }
 
 export default addContacts;
